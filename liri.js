9606d56bcac1162f8acbdc35cb07102ee6c588fa
David Pom <davidpom@gmail.com>
2019-07-12 14:36:58 -0400

    final touches on all functionality, code cleanup and log file bonus

--- liri.js
+++ liri.js
@@ -1,16 +1,18 @@
 var input = process.argv;
 var command = input[2];
 var query = input[3];
-
 require('dotenv').config();
 var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var fs = require('fs');
 var axios = require('axios');
-
+var moment = require('moment');
 var spotify = new Spotify(keys.spotify);
 
-//------------
+// logging data variable
+var dataOut = "";
+
+// Bands in Town Search function
 
 var concertFunc = function() {
 
@@ -20,25 +22,23 @@ var concertFunc = function() {
 
 	axios.get(queryUrl).then(
 	  function(response) {
-	    console.log(response);
-	    console.log(response.data);
-		//Name of the venue
-		//Venue location
-		//Date of the Event (use moment to format this as "MM/DD/YYYY")
+		console.log("\nYou seached for: "+ artist + "\n");
+		console.log("Where: " + response.data[0].venue.name + " in " + response.data[0].venue.city + ", " + response.data[0].venue.country);
+		console.log("When: " + moment(response.data[0].datetime).format('L'));
 
-fs.appendFile('log.txt', "\n" + dataOut, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Content Added!");
-  }
-});
+		dataOut = "\nSearch: " + artist + "\n" + "Where: " + response.data[0].venue.name + " in " + response.data[0].venue.city + ", " + response.data[0].venue.country + "\n" + "When: " + moment(response.data[0].datetime).format('L');
 
+		fs.appendFile('log.txt', "\n" + dataOut, function(err) {
+		  if (err) {
+		    console.log(err);
+		  } else {
+		    console.log("Content Added!");
+		  }
+		});
 	  })
+
 	  .catch(function(error) {
 	    if (error.response) {
-	      // The request was made and the server responded with a status code
-	      // that falls out of the range of 2xx
 	      console.log("---------------Data---------------");
 	      console.log(error.response.data);
 	      console.log("---------------Status---------------");
@@ -46,11 +46,8 @@ fs.appendFile('log.txt', "\n" + dataOut, function(err) {
 	      console.log("---------------Status---------------");
 	      console.log(error.response.headers);
 	    } else if (error.request) {
-	      // The request was made but no response was received
-	      // `error.request` is an object that comes back with details pertaining t$
 	      console.log(error.request);
 	    } else {
-	      // Something happened in setting up the request that triggered an Error
 	      console.log("Error", error.message);
 	    }
 	    console.log(error.config);
@@ -58,47 +55,31 @@ fs.appendFile('log.txt', "\n" + dataOut, function(err) {
 
 };
 
-
+// Spotify song search function
 
 var songFunc = function() {
 	spotify.search({ type: 'track', query: query }, function(err, data) {
   		if (err) {
     			return console.log('Error occurred: ' + err);
   		}
-		console.log(data);
+	        console.log("\nYou seached for: "+ query + "\n");
+		console.log("Track: " +data.tracks.items[0].name);
+		console.log("Artist: " + data.tracks.items[0].artists[0].name);
+		console.log("From the album: " + data.tracks.items[0].album.name);
+		console.log("Link: " + data.tracks.items[0].external_urls.spotify);
+		dataOut = "\nSearch: " + query + "\n" + "Track: " +data.tracks.items[0].name + "\n" + "Artist: " + data.tracks.items[0].artists[0].name + "\n" + "From the album: " + data.tracks.items[0].album.name + "\n" + "Link: " + data.tracks.items[0].external_urls.spotify;
 
-
+		fs.appendFile('log.txt', "\n" + dataOut, function(err) {
+ 		 if (err) {
+		    console.log(err);
+		  } else {
+		    console.log("Content Added!");
+		  }
+		});
 	});
-
-
-
-	//Artist(s)
-	//The song's name
-	//A preview link of the song from Spotify
-	//The album that the song is from
-
-
-	//search: function({
-	//type: 'artist OR album OR track',
-	//query: 'My search query',
-	//limit: 20
-	//},
-	//callback
-	//);
-
-
-
-	//fs.appendFile('log.txt', "\n" + dataOut, function(err) {
-	//  if (err) {
-	//    console.log(err);
-	//  } else {
-	//    console.log("Content Added!");
-	//  }
-	//});
-
 };
 
-
+// Movie search function
 
 var movieFunc = function() {
 
@@ -107,6 +88,7 @@ var movieFunc = function() {
 
 	axios.get(queryUrl).then(
 	  function(response) {
+	        console.log("\nYou seached for: "+ movieName + "\n");
 		console.log("Title: " + response.data.Title);
 		console.log("Year: " + response.data.Year);
 		console.log("The movie's IMDB rating is: " + response.data.Ratings[0].Value);
@@ -116,15 +98,17 @@ var movieFunc = function() {
 		console.log("Plot: " + response.data.Plot);
 		console.log("Cast: " + response.data.Actors);
 
-fs.appendFile('log.txt', "\n" + dataOut, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Content Added!");
-  }
-});
+		dataOut = "\nSearch: " + movieName + "\n" + "Title: " + response.data.Title + "\n" + "Year: " + response.data.Year + "\n" + "The movie's IMDB rating is: " + response.data.Ratings[0].Value + "\n" + "The movie's Rotten Tomatoes rating is: " + response.data.Ratings[1].Value + "\n" + "Country: " + response.data.Country + "\n" + "Language: " + response.data.Language + "\n" + "Plot: " + response.data.Plot + "\n" + "Cast: " + response.data.Actors;
 
+		fs.appendFile('log.txt', "\n" + dataOut, function(err) {
+		  if (err) {
+		    console.log(err);
+		  } else {
+		    console.log("Content Added!");
+		  }
+		});
 	  })
+
 	  .catch(function(error) {
 	    if (error.response) {
 	      console.log("---------------Data---------------");
@@ -142,19 +126,20 @@ fs.appendFile('log.txt', "\n" + dataOut, function(err) {
 	  });
 };
 
+// do-what-it-says function
+
 var whatSaysFunc = function() {
-	 fs.readFile("random.txt", "utf8", function(error, data) {
+	fs.readFile("random.txt", "utf8", function(error, data) {
 
-	  if (error) {
-	    return console.log(error);
-	  }
+	if (error) {
+	  return console.log(error);
+	}
 
-	  console.log(data);
-	  var dataArr = data.split(",");
-	  console.log(dataArr);
-		query = dataArr[1];
-	//here you can run functions on dataArr[1] based on what dataArr[0] says
-	//maybe you have to put each search in a function... that's work!
+	console.log(data);
+	var dataArr = data.split(",");
+	console.log(dataArr);
+	query = dataArr[1];
+
 	switch (true) {
 	    case dataArr[0] == "concert-this":
 		concertFunc();
@@ -173,7 +158,7 @@ var whatSaysFunc = function() {
 
 
 
-//------------
+//Main Switch
 
 switch (true) {
     case input[2] == "concert-this":
@@ -191,6 +176,3 @@ switch (true) {
     default:
         console.log("Please enter a command: concert-this, spotify-this-song, movie-this, or do-what-it-says.");
 };
-
-
-
